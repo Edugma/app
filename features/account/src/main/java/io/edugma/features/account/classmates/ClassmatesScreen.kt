7a47ee85e584e91d9f10c65dc0fef66d267e5c2d
Alexander <like81@yandex.ru>
2022-06-07 13:27:14 +0300

    classmates

--- features/account/src/main/java/io/edugma/features/account/classmates/ClassmatesScreen.kt
+++ features/account/src/main/java/io/edugma/features/account/classmates/ClassmatesScreen.kt
@@ -1,104 +1,106 @@
 package io.edugma.features.account.classmates
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
-import coil.compose.rememberImagePainter
-import coil.transform.CircleCropTransformation
-import io.edugma.domain.account.model.Student
+import io.edugma.features.account.students.Student
 import io.edugma.features.base.core.utils.ClickListener
-import io.edugma.features.base.core.utils.Typed1Listener
-import io.edugma.features.base.elements.ErrorView
+import io.edugma.features.base.core.utils.MaterialTheme3
+import io.edugma.features.base.elements.SpacerWidth
 import org.koin.androidx.compose.getViewModel
 
 @Composable
 fun ClassmatesScreen(viewModel: ClassmatesViewModel = getViewModel()) {
     val state by viewModel.state.collectAsState()
 
-    ClassmatesContent(state,
-    retryListener = {viewModel.loadClassmates()},
-    backListener = {viewModel.exit()},
-    inputListener = {viewModel.inputName(it)})
+    ClassmatesContent(
+        state,
+        retryListener = {viewModel.loadClassmates()},
+        backListener = {viewModel.exit()}
+    )
 }
 
 @Composable
 fun ClassmatesContent(state: ClassmatesState,
                       retryListener: ClickListener,
                       backListener: ClickListener,
-                      inputListener: Typed1Listener<String>
 ) {
-    var name by rememberSaveable { mutableStateOf("") }
-    Scaffold(topBar = {
-        TopAppBar(title = { TextField(value = name,
-            onValueChange = {
-            inputListener.invoke(it)
-            name = it }, label = { Text("ФИО")})},
-            navigationIcon = { IconButton(onClick = { backListener.invoke() }) { Icon(Icons.Filled.ArrowBack, contentDescription = "Назад") } })
-    }) {
-        LazyColumn(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp)
-        ) {
-            if (state.isError && state.data.isEmpty()) {
-                item {
-                    ErrorView {
-                        retryListener.invoke()
-                    }
-                }
-            } else {
-                items(state.filteredData) {
-                    Student(it)
-                    Spacer(modifier = Modifier.padding(2.dp))
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Student(student: Student?) {
-    Card(shape = MaterialTheme.shapes.medium, elevation = 2.dp, modifier = Modifier.fillMaxWidth()) {
-        ConstraintLayout(modifier = Modifier.padding(5.dp)) {
-            val (name, image) = createRefs()
-            Text(text = student?.getFullName().orEmpty(),
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.constrainAs(name) {
-                    start.linkTo(image.end)
-                    end.linkTo(parent.end)
+    Column {
+        ConstraintLayout(Modifier.fillMaxWidth()) {
+            val (content, filter) = createRefs()
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.constrainAs(content) {
+                    linkTo(parent.start, filter.start)
                     width = Dimension.fillToConstraints
+                }) {
+                IconButton(onClick = backListener) {
+                    Icon(
+                        Icons.Filled.ArrowBack,
+                        contentDescription = "Назад"
+                    )
                 }
-                    .padding(start = 10.dp))
-            student?.avatar?.let {
-                Image(
-                    painter = rememberImagePainter(
-                        data = it,
-                        builder = {
-                            transformations(CircleCropTransformation())
-                        }
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(100.dp)
-                        .constrainAs(image) {
-                            start.linkTo(parent.start)
-                            top.linkTo(parent.top)
-                        }
+                SpacerWidth(width = 15.dp)
+                Text(
+                    text = "Однокурсники",
+                    style = MaterialTheme3.typography.titleLarge,
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
-
         }
+        LazyColumn {
+            items(
+                count = state.data.size,
+                key = { state.data[it].id }
+            ) {
+                Student(student = state.data[it])
+            }
+        }
+
     }
 }
+
+//@Composable
+//fun Student(student: Student?) {
+//    Card(shape = MaterialTheme.shapes.medium, elevation = 2.dp, modifier = Modifier.fillMaxWidth()) {
+//        ConstraintLayout(modifier = Modifier.padding(5.dp)) {
+//            val (name, image) = createRefs()
+//            Text(text = student?.getFullName().orEmpty(),
+//                fontWeight = FontWeight.Bold,
+//                modifier = Modifier.constrainAs(name) {
+//                    start.linkTo(image.end)
+//                    end.linkTo(parent.end)
+//                    width = Dimension.fillToConstraints
+//                }
+//                    .padding(start = 10.dp))
+//            student?.avatar?.let {
+//                Image(
+//                    painter = rememberImagePainter(
+//                        data = it,
+//                        builder = {
+//                            transformations(CircleCropTransformation())
+//                        }
+//                    ),
+//                    contentDescription = null,
+//                    modifier = Modifier
+//                        .size(100.dp)
+//                        .constrainAs(image) {
+//                            start.linkTo(parent.start)
+//                            top.linkTo(parent.top)
+//                        }
+//                )
+//            }
+//
+//        }
+//    }
+//}
--- features/account/src/main/java/io/edugma/features/account/classmates/ClassmatesViewModel.kt
+++ features/account/src/main/java/io/edugma/features/account/classmates/ClassmatesViewModel.kt
@@ -5,76 +5,53 @@ import io.edugma.domain.account.model.Student
 import io.edugma.domain.account.repository.PeoplesRepository
 import io.edugma.features.base.core.mvi.BaseMutator
 import io.edugma.domain.base.utils.execute
+import io.edugma.domain.base.utils.onFailure
+import io.edugma.domain.base.utils.onSuccess
+import io.edugma.features.base.core.mvi.BaseViewModel
 import io.edugma.features.base.core.mvi.BaseViewModelFull
 import io.edugma.features.base.core.utils.isNull
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 class ClassmatesViewModel(private val repository: PeoplesRepository) :
-    BaseViewModelFull<ClassmatesState, ClassmatesMutator, Nothing>(ClassmatesState(), ::ClassmatesMutator) {
-        init {
-            loadClassmates()
-        }
+    BaseViewModel<ClassmatesState>(ClassmatesState()) {
+
+    init {
+        loadClassmates()
+    }
 
     fun loadClassmates() {
         viewModelScope.launch {
-            if (!state.value.isLoading) {
-                repository.getClassmates().execute(
-                    onStart = {
-                        mutateState {
-                            setLoading(true)
-                        }
-                    },
-                    onSuccess = {
-                        mutateState {
-                            setData(it)
-                        }
-                    },
-                    onError = {
-                        mutateState {
-                            setError(true)
-                        }
-                    }
-                )
-            }
+            repository.getClassmates()
+                .onStart { setLoading(true) }
+                .onSuccess { setData(it) }
+                .onFailure { setError(true) }
         }
     }
 
-    fun inputName(name: String) {
+    fun setData(data: List<Student>) {
         mutateState {
-            setName(name)
+            state = state.copy(data = data)
+        }
+    }
+
+    fun setLoading(isLoading: Boolean) {
+        mutateState {
+            state = state.copy(isLoading = isLoading, isError = !isLoading && state.isError)
+        }
+    }
+
+    fun setError(isError: Boolean) {
+        mutateState {
+            state = state.copy(isError = isError, isLoading = false)
         }
     }
 
 }
 
 data class ClassmatesState(
-    val data: List<Student?> = emptyList(),
-    val name: String = "",
-    val filteredData: List<Student?> = emptyList(),
+    val data: List<Student> = emptyList(),
     val isLoading: Boolean = false,
+    val placeholders: Boolean = true,
     val isError: Boolean = false
 )
-
-class ClassmatesMutator : BaseMutator<ClassmatesState>() {
-    fun setName(name: String) {
-        state = state.copy(name = name, filteredData = state.data.filter {
-            val predicate = it?.getFullName()?.contains(name, true)
-            predicate == true || predicate.isNull()
-        })
-    }
-
-    fun setData(data: List<Student>) {
-        state = state.copy(data = data, isLoading = false, isError = false, filteredData = data.filter {
-            it.getFullName().contains(state.name, true)
-        })
-    }
-
-    fun setLoading(isLoading: Boolean) {
-        state = state.copy(isLoading = isLoading, isError = !isLoading && state.isError,
-            data = if (state.data.isEmpty() && isLoading) List(5) { null } else state.data)
-    }
-
-    fun setError(isError: Boolean) {
-        state = state.copy(isError = isError, isLoading = false)
-    }
-}
